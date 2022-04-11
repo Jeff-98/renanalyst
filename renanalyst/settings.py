8d88eb5caeac084b68007e480f6ca057e3f8ef1d
Gayuen Wong <gayuen@pc-201-19.customer.ask4.lan>
2022-04-11 22:34:22 +0100

    prep for deploy on heroku

--- renanalyst/settings.py
+++ renanalyst/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import django_on_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2uc=(pja%q+=&7hc$g5&nwcff$ie)30k4b^f=0k1ub-u(@8123'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [renanalyst.herokuapp.com]
 
 
 # Application definition
@@ -125,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate django_on_heroku
+django_on_heroku.settings(locals())
